e356d896e41641bdd86a037a0f80d4306283d137
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 23:35:39 +0800

    up

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-var city_value = "manila";
+var city_value = "";
 
 var valuesb = "we";
 request("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27"+city_value+"%27%29&format=json", function(error, response, body) {
@@ -44,7 +44,7 @@ restService.post('/echo', function(req, res) {
 
     if(actions == "yahooWeatherForecast"){
 
-
+        city_value = "makati";
          result = "Sorry,Not yet avaiable out developer is still fixing it. He has a love problem" + location;
 
 
