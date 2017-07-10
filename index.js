4eada56bd2e5aabca7bee766b4766a9995c3e3f4
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 22:46:34 +0800

    Update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(bodyParser.json());
 
 
 restService.post('/echo', function(req, res) {
-
+    var location ="";
 
     request.get("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27makati%27%29&format=json", function(error, response, body) {
     var jsonObject = JSON.parse(body);
@@ -22,7 +22,7 @@ restService.post('/echo', function(req, res) {
     var query = jsonObject.query;
     var result = query.results;
     var channel = result.channel;
-    var location = channel.location;
+    location = channel.location;
     console.log(location.city);
     //console.log(body);
 });
@@ -63,7 +63,7 @@ restService.post('/echo', function(req, res) {
 
 
     return res.json({
-        speech: result,
+        speech: result + location,
         displayText: result,
         source: 'mel-webhook'
     });
