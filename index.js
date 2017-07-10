c57dc1f5973563b60100acc8cb73204382c55bb2
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 22:44:22 +0800

    Update

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
 
+
+    request.get("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27makati%27%29&format=json", function(error, response, body) {
+    var jsonObject = JSON.parse(body);
+    var jsonval =  JSON.stringify(jsonObject, null,3);
+    var query = jsonObject.query;
+    var result = query.results;
+    var channel = result.channel;
+    var location = channel.location;
+    console.log(location.city);
+    //console.log(body);
+});
+
+
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.mathVal ? req.body.result.parameters.mathVal : "Di ko alam ang pinag sasabi mo.";
 
 
