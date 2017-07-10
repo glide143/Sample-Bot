48dd4703de7a1e6f290cae41348c344f9e5fdfbd
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 23:29:53 +0800

    Update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ restService.use(bodyParser.json());
 var city_value = "manila";
 
 var valuesb = "we";
-request("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27+"city_value"+%27%29&format=json", function(error, response, body) {
+request("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27"+city_value+"%27%29&format=json", function(error, response, body) {
     var jsonObject = JSON.parse(body);
     var jsonval =  JSON.stringify(jsonObject, null,3);
     var query = jsonObject.query;
