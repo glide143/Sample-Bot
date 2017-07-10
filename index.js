8bec4b11dcd646685e7bd6c92700b9d46a716b8c
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 22:54:39 +0800

    Update

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var location ="hi";
 
-    request.post("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27makati%27%29&format=json", function(error, response, body) {
+    request("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27makati%27%29&format=json", function(error, response, body) {
     var jsonObject = JSON.parse(body);
     var jsonval =  JSON.stringify(jsonObject, null,3);
     var query = jsonObject.query;
     var result = query.results;
     var channel = result.channel;
-    location = channel.location;
+    location = channel.location.city;
     console.log(location.city);
     //console.log(body);
 });
