eff2a8f7f62e08559574ed3f8a39a33799f83b16
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-10 22:48:28 +0800

    Update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ restService.use(bodyParser.json());
 
 
 restService.post('/echo', function(req, res) {
-    var location ="";
+    var location ="hi";
 
     request.get("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27makati%27%29&format=json", function(error, response, body) {
     var jsonObject = JSON.parse(body);
