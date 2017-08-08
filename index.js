3730b064df1937e03feec675564df3603dbeeff2
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-08-08 22:14:11 +0800

    UPDATE

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ var city_value = "makati";
 
 
 
-request.get("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27"+city_value+"%27%29&format=json", 
+
+
+restService.post('/echo', function(req, res) {
+
+    request.get("http://query.yahooapis.com/v1/public/yql?q=select+%2A+from+weather.forecast+where+woeid+in+%28select+woeid+from+geo.places%281%29+where+text%3D%27"+city_value+"%27%29&format=json", 
 function(error, response, body) {
    var jsonObject = JSON.parse(body);
     var jsonval =  JSON.stringify(jsonObject, null,3);
@@ -34,7 +38,6 @@ function(error, response, body) {
     valuesb = "Today's forcast for " +location.city+ " is " +condition.temp+ " "+unit.temperature+ "ahrenheit and "+condition.text;
 });
 
-restService.post('/echo', function(req, res) {
 
 
 
